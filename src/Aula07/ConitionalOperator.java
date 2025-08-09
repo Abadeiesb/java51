94e49285a24e56b25b12d051d224babdc0cd6a74
Abade Francisco <francisco.a.filho@iesb.edu.br>
2025-08-09 10:43:33 -0300

    CD operator

---
+++ src/Aula07/ConitionalOperator.java
@@ -0,0 +1,14 @@
+package Aula07;
+
+public class ConitionalOperator {
+    int x = 3;
+    int y;
+
+    public static void main(String[] args) {
+        ConitionalOperator cO = new ConitionalOperator();
+        int getX = cO.x;
+        int getY = cO.y;
+       int result =  (getX >= getY) ?  10 : 20;
+
+    }
+}
