01838f51f64c2ce74df5b1002d9eedec50d608cb
Abade Francisco <francisco.a.filho@iesb.edu.br>
2025-08-12 18:33:01 -0300

    While 2.1

--- src/Aula07/Whiles.java
+++ src/Aula07/Whiles.java
@@ -3,9 +3,14 @@ package Aula07;
 public class Whiles {
     public static void main(String[] args) {
         int i=0;
-        while(i <=99){
+        while(i <=99
+        ){
+            if(i % 2 == 0){
+                System.out.println(i);
+
+            }
             i++;
-            System.out.println(i);
+
 
         }
     }
