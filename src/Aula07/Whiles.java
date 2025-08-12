d6e348c5e47cbc5d5f5bf4afd78c329c6fd9a39e
Abade Francisco <francisco.a.filho@iesb.edu.br>
2025-08-12 18:02:31 -0300

    While

---
+++ src/Aula07/Whiles.java
@@ -0,0 +1,10 @@
+package Aula07;
+
+public class Whiles {
+    public static void main(String[] args) {
+        int i=1;
+        while(i <=100){
+            System.out.println(i++);
+        }
+    }
+}
