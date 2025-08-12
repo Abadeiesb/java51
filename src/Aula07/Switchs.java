bd2d0961b9ba2ac5761aac164499c49e34bf2d65
Abade Francisco <francisco.a.filho@iesb.edu.br>
2025-08-12 17:13:46 -0300

    Switch case 2

--- src/Aula07/Switchs.java
+++ src/Aula07/Switchs.java
@@ -5,10 +5,10 @@ import java.util.Scanner;
 public class Switchs {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Escreve a fruta: ");
-        String fruta = scanner.next();
+       // System.out.println("Escreve a fruta: ");
+        //String fruta = scanner.next();
 
-        switch (fruta) {
+       /* switch (fruta) {
             case "banana":
                 System.out.println("A fruta é uma banana");
                 break;
@@ -19,7 +19,36 @@ public class Switchs {
             default:
                 System.out.println("Isso nem é fruta.");
                 break;
+        }*/
+
+        //int nm1;
+        //int nm2;
+        System.out.println("Escreve os números: ");
+        System.out.println("número 1: ");
+        double result1 = scanner.nextDouble();
+        System.out.println("número 2: ");
+        double result2 = scanner.nextDouble();
+        System.out.println("Escolha uma opção: ");
+        System.out.println("Soma 1: ");
+        System.out.println("Divisão 2: ");
+        int somaDosResults = scanner.nextInt();
+        //int resultResposta = result1 + result2;
+
+
+        switch (somaDosResults) {
+            case 1:
+                System.out.println("Soma: " + (result1 + result2));
+                break;
+            case 2:
+                System.out.println("Divisão : " + (result1 / result2));
+                break;
+
+            default:
+                System.out.println("Também não complica, né!");
+                break;
         }
+
+
 
 
 
