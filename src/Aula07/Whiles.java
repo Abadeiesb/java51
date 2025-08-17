9552445fe2d613e8b7130df19b03b7310e924cf6
Abade Francisco <francisco.a.filho@iesb.edu.br>
2025-08-17 19:47:32 -0300

    for myown solution

--- src/Aula07/Whiles.java
+++ src/Aula07/Whiles.java
@@ -5,24 +5,25 @@ import java.util.Scanner;
 public class Whiles {
     public static void main(String[] args) {
         //Scanner s =  new Scanner(System.in);
-        System.out.println("Enter a number: ");
+        sout("Enter 5 numbers ");
         //int inputNumber=0;
 
         int loop = whilee();
-        System.out.println(" Valor da soma é: "+loop);
+        sout("Valor da soma é: " + loop);
+        //System.out.println(" Valor da soma é: "+loop);
     }
 
     static int whilee() {
         Scanner s2 = new Scanner(System.in);
-        System.out.println("soma: ");
+        System.out.println("soma 1: ");
         int number1= s2.nextInt();
-        System.out.println("soma: ");
+        System.out.println("soma 2: ");
         int number2= s2.nextInt();
-        System.out.println("soma: ");
+        System.out.println("soma 3: ");
         int number3= s2.nextInt();
-        System.out.println("soma: ");
+        System.out.println("soma 4: ");
         int number4= s2.nextInt();
-        System.out.println("soma: ");
+        System.out.println("soma 5: ");
         int number5= s2.nextInt();
         int i=1;
         int sum= number1 + number2 + number3 + number4 + number5;
@@ -30,5 +31,11 @@ public class Whiles {
            // System.out.println(sum);
 
         return sum;
+    }
+
+    static void sout (String texto){
+        System.out.println(texto);
+
+
     }
 }
---
+++ src/aula10/BreakANdContinue.java
@@ -0,0 +1,46 @@
+package aula10;
+
+import java.util.Scanner;
+
+public class BreakANdContinue {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int sum=0,n=0;
+        //sout("User 1");
+
+        for(int i=1 ; i<=10;i++ ){
+            sout("User 1");
+            n = s.nextInt();
+            sum += n;
+            sout("Valor de sum:"+sum);
+            //sout(n);
+        }
+
+
+    }
+
+    static void sout(String text){
+        System.out.println(text);
+    }
+
+           /* sout("User 1");
+        int iputText1 = s.nextInt();
+        sout("User 2");
+        int iputText2 = s.nextInt();
+        sout("User 2");
+        int iputText3 = s.nextInt();
+        sout("User 2");
+        int iputText4 = s.nextInt();
+        sout("User 2");
+        int iputText5 = s.nextInt();
+        sout("User 2");
+        int iputText6 = s.nextInt();
+        sout("User 2");
+        int iputText7 = s.nextInt();
+        sout("User 2");
+        int iputText8 = s.nextInt();
+        sout("User 2");
+       int iputText9 = s.nextInt();
+        sout("User 2");
+        int iputText10 = s.nextInt();*/
+}
